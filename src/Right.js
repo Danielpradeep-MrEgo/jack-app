62fe185580b69374549dbc2b4b7c435c99c5bcba
Danielpradeep-MrEgo <62719869+Danielpradeep-MrEgo@users.noreply.github.com>
2021-04-15 17:53:05 +0530

    Add files via upload

--- src/Right.js
+++ src/Right.js
@@ -1,8 +1,34 @@
 import React from "react";
 import "./Right.css";
+import InfoIcon from "@material-ui/icons/Info";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 
 function Right() {
-	return <div className="right"></div>;
+  // const newsArticle = (heading, subtitle) => {
+  //   <div className="widgets__article">
+  //     <div className="widgets__articleLeft">
+  //       <FiberManualRecordIcon />
+  //     </div>
+  //     <div className="widgets__articleRight">
+  //       <h4>{heading}</h4>
+  //       <p>{subtitle}</p>
+  //     </div>
+  //   </div>;
+  // };
+
+  return (
+    <div className="widgets">
+      {/* <div className="widgets__header">
+        <h2>Creative Studio News</h2>
+        <InfoIcon />
+      </div>
+      {newsArticle("Mr.Ego is now Fire", "Top news - 9099 readers")}
+      {newsArticle("Mr.Ego is now Fire", "Top news - 9099 readers")}
+      {newsArticle("Mr.Ego is now Fire", "Top news - 9099 readers")}
+      {newsArticle("Mr.Ego is now Fire", "Top news - 9099 readers")}
+      {newsArticle("Mr.Ego is now Fire", "Top news - 9099 readers")} */}
+    </div>
+  );
 }
 
 export default Right;
